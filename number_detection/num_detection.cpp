0123738d3677d8bc5d75fa4184117adacd0f26b6
Alifia Ayu Masita <96603308+alifiaayumasita@users.noreply.github.com>
2023-07-15 00:41:01 +0700

    Create num_detection.cpp

---
+++ number_detection/num_detection.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int A =4;
+int B =4;
+
+if (A>B){
+cout<<"Bilangan "<<A<<" adalah bilangan terbesar lho..";
+}else if (A<B || A==B){
+cout<<"Bilangan "<<B<<" adalah bilangan terbesar ya..";
+	}
+return 0;
+}
